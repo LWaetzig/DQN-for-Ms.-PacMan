facf0f5619265c4aabd75ab3edf102d10bea54ec
LWaetzig <lucas@waetzig.net>
2024-05-27 12:35:44 +0200

    feat(utils.py): removed unnecessary imports, add args to function, add function to stack states and converting to tensor

--- src/utils.py
+++ src/utils.py
@@ -1,9 +1,14 @@
 import cv2 as cv
-import gymnasium as gym
 import numpy as np
+import torch
 
 
-def preprocess_state(state: np.array) -> np.array:
+def preprocess_state(
+    state: np.array,
+    stack_states: bool = True,
+    stack_size: int = 4,
+    create_tensor: bool = True,
+) -> torch.tensor:
     """preprocess the state
 
     Args:
@@ -18,5 +23,10 @@ def preprocess_state(state: np.array) -> np.array:
     state = cv.resize(state, (84, 84))
     # normalize the image
     state = state / 255.0
+
+    if stack_states:
+        state = np.stack([state] * stack_size)
+    if create_tensor:
+        state = torch.FloatTensor(state).unsqueeze(0)
 
     return state
